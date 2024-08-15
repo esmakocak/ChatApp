0529d0dd0155686734a6a573af227384fd3914ff
Esma Koçak <esma.kocaakk20@gmail.com>
2024-08-15 23:16:18 +0300

    Implemented Message bubbles

---
+++ ChatApp/Components/MessageBubble.swift
@@ -0,0 +1,42 @@
+//
+//  MessageBubble.swift
+//  ChatApp
+//
+//  Created by Esma Koçak on 15.08.2024.
+//
+
+import SwiftUI
+
+struct MessageBubble: View {
+    var message: Message
+    @State private var showTime = false
+    
+    var body: some View {
+        VStack(alignment: message.isReceived ? .leading : .trailing){
+            HStack{
+                Text(message.text)
+                    .padding()
+                    .background(message.isReceived ? Color("Gray") : Color("Peach"))
+                    .cornerRadius(30)
+            }
+            .frame(maxWidth: 300, alignment: message.isReceived ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+            
+            if showTime {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                    .padding(message.isReceived ? .leading : .trailing, 25)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: message.isReceived ? .leading : .trailing)
+        .padding(message.isReceived ? .leading : .trailing)
+        .padding(.horizontal, 10)
+    }
+}
+
+#Preview {
+    MessageBubble(message: Message(id: "12345", text: "Selam canım benim napıyosun? Görüşmeyeli çok oldu iyisindir inşallah. Caleb'a selam söyle.", isReceived: false, timestamp: Date()))
+}
