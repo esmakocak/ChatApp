2dd0566d8e40ec25192e4ea589d4bffbb5ad820e
Esma Koçak <esma.kocaakk20@gmail.com>
2024-08-15 23:00:44 +0300

    created Message Model

---
+++ ChatApp/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Esma Koçak on 15.08.2024.
+//
+
+import Foundation
+
+struct Message: Identifiable, Codable {
+    var id: String
+    var text: String
+    var isReceived: Bool //if the user is receiver of the message this state will true
+    var timestamp: Date
+}
